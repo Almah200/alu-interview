ec064cee8881ee5cbf9cf192e07d20a346dc6beb
Almarat <arnualmarat@gmail.com>
2023-02-06 15:21:12 +0300

    0

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
 method that calculates the fewest no of opperations to get the result in exactly n H chara in a file
- """
+"""
 
 
 def minOperations(n):
-    """ min operations method"""
-    
+    """
+    min operations method
+    """
     if n <= 1:
         return 0
     operations = 0
